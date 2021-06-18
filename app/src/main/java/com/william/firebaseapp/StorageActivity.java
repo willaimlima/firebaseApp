b8989ac506a42de4e6127b9a12605fec2fd62bfa
Aluno <Aluno>
2021-06-17 22:29:27 -0300

    02 - login e storage

---
+++ app/src/main/java/com/william/firebaseapp/StorageActivity.java
@@ -0,0 +1,13 @@
+package com.william.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StorageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage);
+    }
+}
