b7b2a040802d38adc486389ed39f277fae2746e0
Aluno <Aluno>
2021-07-02 19:35:27 -0300

    11 Notifications

---
+++ app/src/main/java/com/william/firebaseapp/fragment/NotificationFragment.java
@@ -0,0 +1,89 @@
+package com.william.firebaseapp.fragment;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDeepLinkBuilder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.clemilton.firebaseappsala.R;
+import com.william.firebaseapp.NavigationActivity;
+
+import static com.william.firebaseapp.util.App.CHANNEL_1;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NotificationFragment extends Fragment {
+    private NotificationManagerCompat notificationManager;
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout =  inflater.inflate(R.layout.fragment_notification, container, false);
+        notificationManager = NotificationManagerCompat.from(getContext());
+
+        EditText editTitle = layout.findViewById(R.id.frag_notification_title);
+        EditText editMsg = layout.findViewById(R.id.frag_notification_msg);
+        Button  btnSend = layout.findViewById(R.id.frag_notificaion_btn_send);
+        btnSend.setOnClickListener( v -> {
+            String title = editTitle.getText().toString();
+            String msg = editMsg.getText().toString();
+
+            Intent intent = new Intent(getContext(), NavigationActivity.class);
+            /*val pendingIntent = NavDeepLinkBuilder(context)
+                    .setComponentName(YourActivity::class.java)
+                     .setGraph(R.navigation.your_nav_graph)
+                    .setDestination(R.id.your_destination)
+                    .setArguments(bundle)
+                    .createPendingIntent()*/
+            PendingIntent contentIntent = new NavDeepLinkBuilder(getContext())
+                    .setComponentName(NavigationActivity.class)
+                    .setGraph(R.navigation.nav_graph)
+                    .setDestination(R.id.nav_menu_lista_imagens)
+                    .createPendingIntent();
+
+            /*
+            PendingIntent contentIntent = PendingIntent.getActivity(
+                                            getContext(),
+                                            0,
+                                            intent,
+                                            0
+            );*/
+
+            //Criar a notificação
+            Notification notification = new NotificationCompat
+                    .Builder(getContext(),CHANNEL_1)
+                    .setSmallIcon(R.drawable.ic_account_circle_black_24dp)
+                    .setContentTitle(title)
+                    .setContentText(msg)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setContentIntent(contentIntent)
+                    .addAction(R.drawable.ic_account_circle_black_24dp,
+                            "Toast",actionIntent)
+                    .build();
+            notificationManager.notify(1,notification);
+        });
+
+
+
+
+        return layout;
+    }
+}
--- app/src/main/java/com/william/firebaseapp/util/App.java
+++ app/src/main/java/com/william/firebaseapp/util/App.java
@@ -1,38 +1,34 @@
 package com.william.firebaseapp.util;
 
-import android.app.AlertDialog;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 
-import java.nio.channels.Channel;
-
-
-    public  class App extends Application{
-        private static final String  CHANNEL_1="ch_1";
-    }
+public class App extends Application {
+    public static final String CHANNEL_1="ch_1";
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
         createNotificationsChannels();
-
     }
 
     private void createNotificationsChannels(){
-        // verificando se  o celular tem API >= 26
+        //verificando se o celular tem API >= 26
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            //criar canais de notificações
+            //criar canais de notificação
             NotificationChannel channel = new NotificationChannel(
-                    App.CHANNEL_1, "Canal 1", NotificationManager.IMPORTANCE_HIGH
-
+                    CHANNEL_1,
+                    "Canal 1",
+                    NotificationManager.IMPORTANCE_HIGH
             );
-            Channel.setDescription("Este é o canal 1");
+            channel.setDescription("Este é o canal 1");
 
-            // registrar channal
-            NotificationManager manager = getSystemService(NotificationManager.class)
-                    manager.createNotificationChannel(channel);
+            //registrar channel
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
 
         }
     }
 }
+© 2021 GitHub, Inc.
